24b435ce5ebb64f31801c6e84cd1710849c901f1
Blosiu12 <97614945+Blosiu12@users.noreply.github.com>
2024-05-15 07:10:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 
-      document.querySelector('secret').addEventListener('click', () => {
-            fetch('https://api.example.com/secret') // Zmień na właściwy URL API
+      document.querySelector('#secret').addEventListener('click', () => {
+            fetch('https://cors.szurek.local/10/') 
                 .then(response => response.json())
                 .then(data => {
-                    document.getElementById('secret').textContent = data.secret;
+                    document.querySelector('#secret').textContent = data.secret;
                 })
                 .catch(error => {
-                    console.error('Error fetching the secret:', error);
+                    console.error('Error:', error);
                 });
         });
