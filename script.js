41bacfc8e815382c1012d72509941c7f8b8a40ee
Blosiu12 <97614945+Blosiu12@users.noreply.github.com>
2024-05-15 13:01:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
             fetch('https://cors.szurek.local/10/') 
                 .then(response => response.json())
                 .then(data => {
-                    document.querySelector('#secret-text').textContent = data.secret;
+                    document.querySelector('#secret-text').textContent = data.sekret;
                 })
                 .catch(error => {
                     console.error('Error:', error);
